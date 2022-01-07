2f5c7be7634d92c381ff692bee94451610f8ec34
Santi <83207233+ssayol11@users.noreply.github.com>
2022-01-07 12:58:04 +0100

    Update greedy.cc

--- greedy.cc
+++ greedy.cc
@@ -1,41 +1,66 @@
+#include <cstdlib>
 #include <ctime>
+#include <fstream>
 #include <iostream>
 #include <map>
+#include <string>
 #include <vector>
 using namespace std;
 
-int main()
-{
-    cout.setf(ios::fixed);
-    cout.precision(4);
-    double now();
+string fitxer_sortida;
+int num_pel;
+int num_res;
+int num_sal;
+vector<string> pel;
+map<string, int> pel_num;
+vector<vector<int>> res;
+vector<string> sal;
 
-    int num_pel;
-    cin >> num_pel;
-    vector<string> pel(num_pel);
+double now()
+{
+    return clock() / double(CLOCKS_PER_SEC);
+}
+
+void llegir(const string& dades)
+{
+    ifstream in(dades);
+    if (in.fail()) {
+        cout << "Error: Any instance has been found" << endl;
+        exit(1);
+    }
+    in >> num_pel;
+    pel = vector<string>(num_pel);
     map<string, int> pel_num;
     for (int i = 0; i < num_pel; ++i) {
-        cin >> pel[i];
+        in >> pel[i];
         pel_num.insert({ pel[i], i });
     }
-
-    int num_res;
-    cin >> num_res;
+    in >> num_res;
     pair<string, string> restric;
-    vector<vector<int>> res(num_pel);
+    res = vector<vector<int>>(num_pel);
     for (int i = 0; i < num_res; ++i) {
-        cin >> restric.first >> restric.second;
+        in >> restric.first >> restric.second;
         res[pel_num.find(restric.first)->second].push_back(pel_num.find(restric.second)->second);
         res[pel_num.find(restric.second)->second].push_back(pel_num.find(restric.first)->second);
     }
-
-    int num_sal;
-    cin >> num_sal;
-    vector<string> sal(num_sal);
+    in >> num_sal;
+    sal = vector<string>(num_sal);
     for (int i = 0; i < num_sal; ++i) {
-        cin >> sal[i];
+        in >> sal[i];
     }
+}
 
+int main(int argc, char** argv)
+{
+    double inici = now();
+    if (argc != 3) {
+        cout << "Syntax: "
+             << "exh.exe "
+             << "instance.inp "
+             << "solution.txt" << endl;
+        exit(1);
+    }
+    llegir(argv[1]);
     vector<vector<int>> plan(num_sal);
     int num_rep = 0;
     vector<bool> rep(num_pel, true);
@@ -65,14 +90,21 @@ int main()
         }
     }
 
-    cout << clock() / double(CLOCKS_PER_SEC) << endl;
+    double final = now();
+    fitxer_sortida = argv[2];
+    ofstream fitxer;
+    fitxer.setf(ios::fixed);
+    fitxer.precision(1);
+    fitxer.open(fitxer_sortida);
+    fitxer << final - inici << "\n";
     int num_dies = plan[0].size();
-    cout << num_dies << endl;
+    fitxer << num_dies << "\n";
     for (int o = 0; o < num_dies; ++o) {
         for (int i = 0; i < num_sal; ++i) {
             if (plan[i][o] != -1) {
-                cout << pel[plan[i][o]] << ' ' << o + 1 << ' ' << sal[i] << endl;
+                fitxer << pel[plan[i][o]] << ' ' << o + 1 << ' ' << sal[i] << "\n";
             }
         }
     }
+    fitxer.close();
 }
